d6c6424f043beff0e3a8ae5676041d63ae27345e
Elliott Arnold <46078321+si3mshady@users.noreply.github.com>
2023-10-06 03:11:21 -0500

    Add files via upload

--- database.py
+++ database.py
@@ -10,13 +10,12 @@ def main():
     )
     cursor = cnx.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS test(id INTEGER(64) PRIMARY KEY, name VARCHAR(255))")
+    cursor.execute("CREATE TABLE food_items (  id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, price DECIMAL(10, 2) NOT NULL);")
 
-    cursor.execute("INSERT INTO test VALUES (2, 'bla')")
-    cursor.execute("INSERT INTO test VALUES (3, 'blabla')")
-    cursor.execute("INSERT INTO test VALUES (4, 'blablabla')")
+    cursor.execute("INSERT INTO food_items (name, price) VALUES ('Donut', 2.50), ('Pigs in a Blanket', 4.00),('Burrito', 5.50),('Shake', 3.00),('Sandwich', 4.50), ('Pancake', 3.25);")
+    
 
-    cursor.execute("SELECT * FROM test")
+    cursor.execute("SELECT * FROM food_items")
     for row in cursor.fetchall():
         print(row)
     
